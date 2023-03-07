da4b5e1f6362384d55d7cdd8c108efbef93fc6c7
Adebobola01 <m.oyedunmade@gmail.com>
2023-03-07 01:14:00 +0100

    fixed hero page responsiveness

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import logoImg from "../../assets/images/big3-logo.png"
 import "./Logo.scss"
 const logo = props => {
     return (
         <div className="logo">
-            <NavLink to="/" >
+            <Link to="/" >
                 <img
                     src={logoImg}
                     alt="logo"
                     className="logo__icon"
                 />
                 <span class="logo__title">Big3</span>
-            </NavLink>
+            </Link>
         </div>
     )
 }
