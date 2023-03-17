c9ac7d25a26b66487b8e5aaf75d806fe49e06106
Adebobola01 <m.oyedunmade@gmail.com>
2023-03-17 22:48:34 +0100

    feat: added trending section

---
+++ src/components/Trending/index.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import "./Trending.scss";
+import akatsuki from "../../assets/images/akatsuki.png"
+
+const Trending = props => {
+
+    return (
+        <section className="Trending" >
+            <span>
+                Trending
+            </span>
+            <div className="Trending-nfts">
+                <div className="Trending-nft">
+                    <img src={akatsuki} alt="trending img" />
+                    <div>
+                        <span>Azuki #12</span>
+                        <a>Adebobola</a>
+                    </div>
+                </div>
+                <div className="Trending-nft">
+                    <img src={akatsuki} alt="trending img" />
+                    <div>
+                        <span>Azuki #12</span>
+                        <a>Adebobola</a>
+                    </div>
+                </div>
+                <div className="Trending-nft">
+                    <img src={akatsuki} alt="trending img" />
+                    <div>
+                        <span>Azuki #12</span>
+                        <a>Adebobola</a>
+                    </div>
+                </div>
+                <div className="Trending-nft">
+                    <img src={akatsuki} alt="trending img" />
+                    <div>
+                        <span>Azuki #12</span>
+                        <a>Adebobola</a>
+                    </div>
+                </div>
+                <div className="Trending-nft">
+                    <img src={akatsuki} alt="trending img" />
+                    <div>
+                        <span>Azuki #12</span>
+                        <a>Adebobola</a>
+                    </div>
+                </div>
+                <div className="Trending-nft">
+                    <img src={akatsuki} alt="trending img" />
+                    <div>
+                        <span>Azuki #12</span>
+                        <a>Adebobola</a>
+                    </div>
+                </div>
+                <div className="Trending-nft">
+                    <img src={akatsuki} alt="trending img" />
+                    <div>
+                        <span>Azuki #12</span>
+                        <a>Adebobola</a>
+                    </div>
+                </div>
+                <div className="Trending-nft">
+                    <img src={akatsuki} alt="trending img" />
+                    <div>
+                        <span>Azuki #12</span>
+                        <a>Adebobola</a>
+                    </div>
+                </div>
+                <div className="Trending-nft">
+                    <img src={akatsuki} alt="trending img" />
+                    <div>
+                        <span>Azuki #12</span>
+                        <a>Adebobola</a>
+                    </div>
+                </div>
+                <div className="Trending-nft">
+                    <img src={akatsuki} alt="trending img" />
+                    <div>
+                        <span>Azuki #12</span>
+                        <a>Adebobola</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Trending;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Hero from "../../components/Hero";
-
+import Trending from "../../components/Trending";
 const home = (props) => {
 
 
     return (
-        <Hero/>
+        <>
+            <Hero />
+            <Trending/>
+        </>
     )
 }
 
