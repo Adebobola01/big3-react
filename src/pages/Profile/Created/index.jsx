b9e78d710f71fdb321bb48819cccb3580e4fa390
Adebobola01 <m.oyedunmade@gmail.com>
2023-03-29 14:55:28 +0100

    fixed profile sub pages

--- src/pages/Profile/Created/index.jsx
+++ src/pages/Profile/Created/index.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import "./Created.scss";
 
 const Created = props => {
+    const [created, setCreated] = useState([]);
+
+    useEffect(() => {
+        setCreated([]);
+        console.log("useEffect");
+    }, [])
+    const content = created.length !== 0 ? <p>created nfts</p> : <div className="profile-content"> <span> No Content Available </span> </div>;
     return (
-        <p>created nfts</p>
+        <>
+            {content}
+        </>
     )
 }
 
--- src/pages/Profile/Listed/index.jsx
+++ src/pages/Profile/Listed/index.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./Listed.scss";
 
 const Listed = props => {
+    const [listed, setListed] = useState([])
+    useEffect(() => {
+        setListed([]);
+    }, []);
+
+
+    console.log(listed);
+    const content = listed.length !== 0 ? <p>Listed Nfts</p> : <div className="profile-content"> <span> No Content Available </span> </div>
+    console.log("here")
     return (
-        <p>Listed Nfts</p>
+        <>
+            {content}   
+        </>
     )
 }
 
