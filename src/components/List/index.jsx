ce5bcbf6c02a9e8ebba8840ff9dcfadfb74c16a4
Adebobola01 <m.oyedunmade@gmail.com>
2023-04-10 08:25:44 +0100

    fix: list container

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import "./List.scss";
+import Backdrop from "../Backdrop";
 // import ethUrl from "../../assets/images/ethereum-eth-logo.svg";
+import ethUrl from "../../assets/images/madara.png"
 
 
 const List = props => {
@@ -12,34 +14,37 @@ const List = props => {
         })
     }
     return (
-        <div className="list-box">
-            <div className="list__container">
-                <span className="list-close">X</span>
-                <div className="list-preview">
-                    <img src={listDetails.image} />
-                </div>
-                <div className="list__price">
-                    <p>Price</p>
-                    <div className="list__price-container"> 
-                        <input className="list__price--input" placeholder="Amount" type="number"></input>
-                        <img src="{ ethUrl}" />
+        <>
+            <Backdrop open={true} />
+            <div className="list-box">
+                <div className="list__container">
+                    <span className="list-close">X</span>
+                    <div className="list-preview">
+                        <img src={ethUrl} className="list-preview__image"/>
                     </div>
+                    <div className="list__price">
+                        <p>Price</p>
+                        <div className="list__price-container"> 
+                            <input className="list__price--input" placeholder="Amount" type="number"></input>
+                            <img src="{ ethUrl}" />
+                        </div>
+                    </div>
+                    <div className="list__price">
+                    <p>Duration</p>
+                    <div className="list__price-container"> 
+                        <input className="list__duration--input" placeholder="24" type="number"></input>
+                        <select name="duration" id="duration" className="list__duration--select">
+                            <option value="hours">Hours</option>
+                            <option value="days">Days</option>
+                            <option value="weeks">Weeks</option>
+                            <option value="months">Months</option>
+                        </select>
+                    </div>
+                    </div>
+                    <button className="list-btn">List</button>
                 </div>
-                <div className="list__price">
-                <p>Duration</p>
-                <div className="list__price-container"> 
-                    <input className="list__duration--input" placeholder="24" type="number"></input>
-                    <select name="duration" id="duration" className="list__duration--select">
-                        <option value="hours">Hours</option>
-                        <option value="days">Days</option>
-                        <option value="weeks">Weeks</option>
-                        <option value="months">Months</option>
-                    </select>
-                </div>
-                </div>
-                <button className="list-btn">List</button>
             </div>
-        </div>
+        </>
     )
 }
 
