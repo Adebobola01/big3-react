46f6e98b444f1daf909fe93c06610836bcef2723
Adebobola01 <m.oyedunmade@gmail.com>
2023-05-10 11:45:52 +0100

    fix: trending section

--- src/components/Trending/index.tsx
+++ src/components/Trending/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Trending.scss";
-// import "akatsuki" from "../../assets/images/akatsuki.png"
+import akatsuki from "../../assets/images/akatsuki.png"
 
 const Trending = (props: any) => {
 
@@ -11,70 +11,70 @@ const Trending = (props: any) => {
             </span>
             <div className="Trending-nfts">
                 <div className="Trending-nft">
-                    <img src={"akatsuki"} alt="trending img" />
+                    <img src={akatsuki} alt="trending img" />
                     <div>
                         <span>Azuki #12</span>
                         <a>Adebobola</a>
                     </div>
                 </div>
                 <div className="Trending-nft">
-                    <img src={"akatsuki"} alt="trending img" />
+                    <img src={akatsuki} alt="trending img" />
                     <div>
                         <span>Azuki #12</span>
                         <a>Adebobola</a>
                     </div>
                 </div>
                 <div className="Trending-nft">
-                    <img src={"akatsuki"} alt="trending img" />
+                    <img src={akatsuki} alt="trending img" />
                     <div>
                         <span>Azuki #12</span>
                         <a>Adebobola</a>
                     </div>
                 </div>
                 <div className="Trending-nft">
-                    <img src={"akatsuki"} alt="trending img" />
+                    <img src={akatsuki} alt="trending img" />
                     <div>
                         <span>Azuki #12</span>
                         <a>Adebobola</a>
                     </div>
                 </div>
                 <div className="Trending-nft">
-                    <img src={"akatsuki"} alt="trending img" />
+                    <img src={akatsuki} alt="trending img" />
                     <div>
                         <span>Azuki #12</span>
                         <a>Adebobola</a>
                     </div>
                 </div>
                 <div className="Trending-nft">
-                    <img src={"akatsuki"} alt="trending img" />
+                    <img src={akatsuki} alt="trending img" />
                     <div>
                         <span>Azuki #12</span>
                         <a>Adebobola</a>
                     </div>
                 </div>
                 <div className="Trending-nft">
-                    <img src={"akatsuki"} alt="trending img" />
+                    <img src={akatsuki} alt="trending img" />
                     <div>
                         <span>Azuki #12</span>
                         <a>Adebobola</a>
                     </div>
                 </div>
                 <div className="Trending-nft">
-                    <img src={"akatsuki"} alt="trending img" />
+                    <img src={akatsuki} alt="trending img" />
                     <div>
                         <span>Azuki #12</span>
                         <a>Adebobola</a>
                     </div>
                 </div>
                 <div className="Trending-nft">
-                    <img src={"akatsuki"} alt="trending img" />
+                    <img src={akatsuki} alt="trending img" />
                     <div>
                         <span>Azuki #12</span>
                         <a>Adebobola</a>
                     </div>
                 </div>
                 <div className="Trending-nft">
-                    <img src={"akatsuki"} alt="trending img" />
+                    <img src={akatsuki} alt="trending img" />
                     <div>
                         <span>Azuki #12</span>
                         <a>Adebobola</a>
