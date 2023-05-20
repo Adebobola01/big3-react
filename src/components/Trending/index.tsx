f42aa46b5235e483ac5eb1fd6c150b92cd2506eb
Adebobola01 <m.oyedunmade@gmail.com>
2023-05-20 23:35:16 +0100

    feat: added trending feature

--- src/components/Trending/index.tsx
+++ src/components/Trending/index.tsx
@@ -1,85 +1,165 @@
 import React from "react";
-import "./Trending.scss";
+import styles from "./Trending.module.scss";
 import akatsuki from "../../assets/images/akatsuki.png"
+import { NavLink } from "react-router-dom";
 
 const Trending = (props: any) => {
 
     return (
-        <section className="Trending" >
-            <span>
-                Trending Collections
-            </span>
-            <div className="Trending-nfts">
-                <div className="Trending-nft">
-                    <img src={akatsuki} alt="trending img" />
-                    <div>
-                        <span>Azuki #12</span>
-                        <a>Adebobola</a>
-                    </div>
-                </div>
-                <div className="Trending-nft">
-                    <img src={akatsuki} alt="trending img" />
-                    <div>
-                        <span>Azuki #12</span>
-                        <a>Adebobola</a>
-                    </div>
-                </div>
-                <div className="Trending-nft">
-                    <img src={akatsuki} alt="trending img" />
-                    <div>
-                        <span>Azuki #12</span>
-                        <a>Adebobola</a>
-                    </div>
-                </div>
-                <div className="Trending-nft">
-                    <img src={akatsuki} alt="trending img" />
-                    <div>
-                        <span>Azuki #12</span>
-                        <a>Adebobola</a>
-                    </div>
-                </div>
-                <div className="Trending-nft">
-                    <img src={akatsuki} alt="trending img" />
-                    <div>
-                        <span>Azuki #12</span>
-                        <a>Adebobola</a>
-                    </div>
-                </div>
-                <div className="Trending-nft">
-                    <img src={akatsuki} alt="trending img" />
-                    <div>
-                        <span>Azuki #12</span>
-                        <a>Adebobola</a>
-                    </div>
-                </div>
-                <div className="Trending-nft">
-                    <img src={akatsuki} alt="trending img" />
-                    <div>
-                        <span>Azuki #12</span>
-                        <a>Adebobola</a>
-                    </div>
-                </div>
-                <div className="Trending-nft">
-                    <img src={akatsuki} alt="trending img" />
-                    <div>
-                        <span>Azuki #12</span>
-                        <a>Adebobola</a>
-                    </div>
-                </div>
-                <div className="Trending-nft">
-                    <img src={akatsuki} alt="trending img" />
-                    <div>
-                        <span>Azuki #12</span>
-                        <a>Adebobola</a>
-                    </div>
-                </div>
-                <div className="Trending-nft">
-                    <img src={akatsuki} alt="trending img" />
-                    <div>
-                        <span>Azuki #12</span>
-                        <a>Adebobola</a>
-                    </div>
-                </div>
+        <section className={styles.trending} >
+            <header className={styles.trending_header}>
+                <button className={`${styles.trending_btn} ${styles.trending_active}`}>
+                    Trending
+                </button>
+                <button className={styles.trending_btn}>
+                    Top
+                </button>
+            </header>
+            <div className={styles.trending_body}>
+                <table className={styles.trending_table} >
+                    <tr>
+                        <th className={styles.trending_index} >#</th>
+                        <th className={styles.trending_collections}>collection</th>
+                        <th className={styles.trending_volume}>volume</th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>
+                            <div className={styles.trending_collection} >
+                                <img src={akatsuki} alt="img" />
+                                <div>
+                                    <span>Akatsuki</span>
+                                    <p>Floor: 0.3 ETH</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>7.8 ETH</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>
+                            <div className={styles.trending_collection} >
+                                <img src={akatsuki} alt="img" />
+                                <div>
+                                    <span>Akatsuki</span>
+                                    <p>Floor: 0.3 ETH</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>7.8 ETH</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>
+                            <div className={styles.trending_collection} >
+                                <img src={akatsuki} alt="img" />
+                                <div>
+                                    <span>Akatsuki</span>
+                                    <p>Floor: 0.3 ETH</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>7.8 ETH</td>
+                    </tr>
+                    <tr>
+                        <td>4</td>
+                        <td>
+                            <div className={styles.trending_collection} >
+                                <img src={akatsuki} alt="img" />
+                                <div>
+                                    <span>Akatsuki</span>
+                                    <p>Floor: 0.3 ETH</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>7.8 ETH</td>
+                    </tr>
+                    <tr>
+                        <td>5</td>
+                        <td>
+                            <div className={styles.trending_collection} >
+                                <img src={akatsuki} alt="img" />
+                                <div>
+                                    <span>Akatsuki</span>
+                                    <p>Floor: 0.3 ETH</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>7.8 ETH</td>
+                    </tr>
+                </table>
+                <table className={styles.trending_table} >
+                    <tr>
+                        <th className={styles.trending_index} >#</th>
+                        <th className={styles.trending_collections}>collection</th>
+                        <th className={styles.trending_volume}>volume</th>
+                    </tr>
+                    <tr>
+                        <td>6</td>
+                        <td>
+                            <div className={styles.trending_collection} >
+                                <img src={akatsuki} alt="img" />
+                                <div>
+                                    <span>Akatsuki</span>
+                                    <p>Floor: 0.3 ETH</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>7.8 ETH</td>
+                    </tr>
+                    <tr>
+                        <td>7</td>
+                        <td>
+                            <div className={styles.trending_collection} >
+                                <img src={akatsuki} alt="img" />
+                                <div>
+                                    <span>Akatsuki</span>
+                                    <p>Floor: 0.3 ETH</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>7.8 ETH</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>
+                            <div className={styles.trending_collection} >
+                                <img src={akatsuki} alt="img" />
+                                <div>
+                                    <span>Akatsuki</span>
+                                    <p>Floor: 0.3 ETH</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>7.8 ETH</td>
+                    </tr>
+                    <tr>
+                        <td>8</td>
+                        <td>
+                            <div className={styles.trending_collection} >
+                                <img src={akatsuki} alt="img" />
+                                <div>
+                                    <span>Akatsuki</span>
+                                    <p>Floor: 0.3 ETH</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>7.8 ETH</td>
+                    </tr>
+                    <tr>
+                        <td>10</td>
+                        <td>
+                            <div className={styles.trending_collection} >
+                                <img src={akatsuki} alt="img" />
+                                <div>
+                                    <span>Akatsuki</span>
+                                    <p>Floor: 0.3 ETH</p>
+                                </div>
+                            </div>
+                        </td>
+                        <td>7.8 ETH</td>
+                    </tr>
+                </table>
             </div>
         </section>
     )
